51362c4b26d1270d930c109be3ba64c4ed41296d
webpawan <pawansaini66477@gmail.com>
2023-04-09 23:52:29 +0530

    basic setup of my portfoiol

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+function App() {
+  return (
+      <h1 className="text-3xl font-bold underline text-center text-red-700 bg-slate-800">
+        Hello world!
+      </h1>
+  );
+}
+
+export default App;
