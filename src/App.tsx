4ef866bc6317220a5628a12ee5e2b1bb4f5f478a
webpawan <pawansaini66477@gmail.com>
2023-04-26 23:56:33 +0530

    build is ready loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import About from "./components/pages/about/About";
 import Home from "./components/pages/home/Home";
 import Project from "./components/pages/projects/Project";
 
 function App() {
- 
+  const [isReady, setIsReady] = useState(false);
 
+  useEffect(() => {
+    // Simulate website loading delay
+    setTimeout(() => {
+      setIsReady(true);
+    }, 3000);
+  }, []);
+
+  if(!isReady){
+    return <h1 className="flex justify-center items-center h-screen w-screen text-lg">loading...</h1>
+  }
 
   return (
     <div>
