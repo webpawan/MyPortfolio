9a45a503cd552e65c4b20909c65fe8204f49afb0
webpawan <pawansaini66477@gmail.com>
2023-05-08 23:18:42 +0530

    remove old loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,8 @@ import Home from "./components/pages/home/Home";
 import Project from "./components/pages/projects/Project";
 
 function App() {
-  const [isReady, setIsReady] = useState(false);
 
-  useEffect(() => {
-    // Simulate website loading delay
-    setTimeout(() => {
-      setIsReady(true);
-    }, 2000);
-  }, []);
-
-  if(!isReady){
-    return <h1 className="flex justify-center items-center h-screen w-screen text-lg">loading...</h1>
-  }
+ 
 
   return (
     <>
